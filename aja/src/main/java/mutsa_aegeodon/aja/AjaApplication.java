8d9bfe3551e806d2ec6d01c9edf3d4c3e3bc9af4
고여경 <2yokyung@naver.com>
2025-07-24 13:01:58 +0900

    [Feat]: init setting

---
+++ aja/src/main/java/mutsa_aegeodon/aja/AjaApplication.java
@@ -0,0 +1,13 @@
+package mutsa_aegeodon.aja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaApplication.class, args);
+	}
+
+}
---
+++ aja/src/test/java/mutsa_aegeodon/aja/AjaApplicationTests.java
@@ -0,0 +1,15 @@
+package mutsa_aegeodon.aja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
